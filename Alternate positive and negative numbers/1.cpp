d1a4d9f1887f4839e8a6594876588e1ad63924d3
ASHISH ADHIKARI <56301864+Ashishadhikari123@users.noreply.github.com>
2022-07-14 20:28:02 +0530

    using 2 extra vector

---
+++ Alternate positive and negative numbers/1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    vector<int>pos,neg;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]<0)
+	            neg.push_back(arr[i]);
+	        else
+	            pos.push_back(arr[i]);
+	    }
+	    int i=0,j=0,k=0;
+	    while(i<pos.size() && j<neg.size())
+	    {
+	        arr[k++]=pos[i++];
+	        arr[k++]=neg[j++];
+	    }
+	    while(i<pos.size())
+	        arr[k++]=pos[i++];
+	    while(j<neg.size())
+	        arr[k++]=neg[j++];
+	}
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
